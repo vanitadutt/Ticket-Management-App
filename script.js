87477c274bd703a096bb553c406f64bffbda6c78
vanitadutt <vanitadutt96@gmail.com>
2022-06-17 21:28:29 +0530

    remove-btn

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ let addBtn = document.querySelector(".add-btn");
 let modalCont = document.querySelector(".modal-cont");
 let mainCont = document.querySelector(".main-cont");
 let taskArea = document.querySelector(".textArea-cont");
+
+let removeBtn = document.querySelector('.remove-btn')
 let addFlag = false;
+let removeFlag = false;
 
 addBtn.addEventListener("click", function () {
   //display the modal
@@ -37,4 +40,26 @@ function createTicket(ticketTask) {
     </div>`;
 
   mainCont.append(ticketCont);
+  handleRemoval(ticketCont);
+}
+
+removeBtn.addEventListener('click',function(){
+  removeFlag = !removeFlag;
+
+  if(removeFlag === true){
+    removeBtn.style.color = 'red';
+
+  }
+  else{
+    removeBtn.style.color = 'white'
+  }
+})
+
+function handleRemoval(ticket){
+
+    ticket.addEventListener('click',function(){
+      if(!removeFlag) return; //if return
+      ticket.remove(); // else remove from ui
+    })
+    
 }
