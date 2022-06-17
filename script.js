7d8dac2e460310482178f5ad10751cb974c41194
vanitadutt <vanitadutt96@gmail.com>
2022-06-17 23:01:17 +0530

    lock

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ let addBtn = document.querySelector(".add-btn");
 let modalCont = document.querySelector(".modal-cont");
 let mainCont = document.querySelector(".main-cont");
 let taskArea = document.querySelector(".textArea-cont");
+let removeBtn = document.querySelector(".remove-btn");
+let allPriorityColors = document.querySelectorAll(".priority-color");
 
-let removeBtn = document.querySelector('.remove-btn')
+let colors = ["lightpink", "lightgreen", "lightblue", "black"];
+let modalPriorityColor = colors[colors.length-1];
 let addFlag = false;
 let removeFlag = false;
+let lockClass = 'fa-lock';
+let unlockClass = 'fa-lock-open'
 
 addBtn.addEventListener("click", function () {
   //display the modal
@@ -17,49 +22,87 @@ addBtn.addEventListener("click", function () {
   }
 });
 
+// Select the priority color of the task
+
+allPriorityColors.forEach(function (colorElem) {
+  colorElem.addEventListener('click', function (e) {
+    allPriorityColors.forEach(function (priorityColorElem) {
+      priorityColorElem.classList.remove('active');
+    });
+    colorElem.classList.add('active');
+    modalPriorityColor = colorElem.classList[0];
+  });
+});
+
+// Generating a ticket
+
 modalCont.addEventListener("keydown", function (e) {
   let key = e.key;
 
   if (key == "Shift") {
-    createTicket(taskArea.value);
+    createTicket(taskArea.value,modalPriorityColor);
     modalCont.style.display = "none";
     addFlag = false;
     taskArea.value = "";
   }
 });
 
-function createTicket(ticketTask) {
+function createTicket(ticketTask,ticketColor) {
   let id = shortid();
   let ticketCont = document.createElement("div");
   ticketCont.setAttribute("class", "ticket-cont");
-  ticketCont.innerHTML = ` <div class="ticket-color"></div>
+  ticketCont.innerHTML = ` <div class="ticket-color ${ticketColor}"></div>
     <div class="ticket-id">#${id}</div>
     <div class="task-area">${ticketTask}</div>
     <div class="ticket-lock">
       <i class="fa-solid fa-lock"></i>
     </div>`;
-
+  
   mainCont.append(ticketCont);
   handleRemoval(ticketCont);
+  handleLock(ticketCont)
 }
 
-removeBtn.addEventListener('click',function(){
+removeBtn.addEventListener("click", function () {
   removeFlag = !removeFlag;
 
-  if(removeFlag === true){
-    removeBtn.style.color = 'red';
-
+  if (removeFlag === true) {
+    removeBtn.style.color = "red";
+  } else {
+    removeBtn.style.color = "white";
   }
-  else{
-    removeBtn.style.color = 'white'
-  }
-})
+});
 
-function handleRemoval(ticket){
-
-    ticket.addEventListener('click',function(){
-      if(!removeFlag) return; //if return
-      ticket.remove(); // else remove from ui
-    })
-    
+function handleRemoval(ticket) {
+  ticket.addEventListener("click", function () {
+    if (!removeFlag) return; //if return
+    ticket.remove(); // else remove from ui
+  });
 }
+
+// Lock and unlock tickets
+
+function handleLock(ticket){
+
+  let ticketLockElem = ticket.querySelector('.ticket-lock');
+  let ticketLockIcon = ticketLockElem.children[0];
+  let ticketTaskArea = ticket.querySelector('.task-area');
+
+  ticketLockIcon.addEventListener('click',function(){
+      if(ticketLockIcon.classList.contains(lockClass)){
+        ticketLockIcon.classList.remove(lockClass);
+        ticketLockIcon.classList.add(unlockClass);
+        ticketTaskArea.setAttribute('contenteditable','true');
+      }
+      else{
+        ticketLockIcon.classList.remove(unlockClass);
+        ticketLockIcon.classList.add(lockClass);
+        ticketTaskArea.setAttribute('contenteditable','false');
+
+      }
+  })
+
+}
+
+//
+
